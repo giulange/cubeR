836a68cf219faa7b9473a600752cb877e5f6ce33
Mateusz Żółtak <zozlak@zozlak.org>
2020-02-06 13:15:42 +0100

    Atmospheric correstion scheduling rewritten:
    * check data availbility on bands level
    * avoid scheduling ESA-reprocessed versions of the same acqisition
    * more fine-grained granules selection in case of cloud cover

--- vignettes/schedule_rawprocessing.R
+++ vignettes/schedule_rawprocessing.R
@@ -1,87 +1,122 @@
 library(sentinel2)
 library(dplyr)
-stop('przepisać na wyszukiwanie images i uwzględnić istnienie wszystkich wymaganych band-ów')
 
 date2months = function(x, k = 1) {
   return(as.integer(substr(x, 1, 4)) * 12 + floor(as.integer(substr(x, 6, 7)) / k) * k - 1)
 }
-months2date = function(x, k = 1) {
-  return(sprintf('%04d-%02d', floor(x / 12), 1 + (x %% 12) * k))
+months2date = function(x, k = 1, end = FALSE) {
+  x = x + if_else(end, 1L, 0L)
+  date = as.Date(sprintf('%04d-%02d-01', floor(x / 12), 1 + (x %% 12) * k)) - if_else(end, 1L, 0L)
+  return(as.character(date))
 }
 
+bands = c('B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B11', 'B12', 'SCL', 'LAI', 'TCI', 'FAPAR', 'FCOVER')
+roiEu = S2_query_roi(regionId = 'EU_cube')
+monthMin = date2months('2020-01')
+monthMax = date2months('2020-01')
 S2_initialize_user('landsupport', 'CbYwN9cNvp')
 
-maxGranules = 1 # per orbit
-maxCc = 0.2
-monthMin = date2months('2019-01') #min(g$month)
-monthMax = date2months('2019-12') #max(g$month)
-
-g = as.tbl(S2_query_granule(regionId = '%_cube', cloudCovMax = 100, dateMin = paste0(months2date(monthMin), '-01'), dateMax = paste0(months2date(monthMax), '-01')))
-g = g %>%
-  mutate(
-    month = date2months(date),
-    cube = !is.na(url)
-  )
-save(g, file = 'all_granules.RData')
-
-months = tibble(month = seq(monthMin, monthMax)) %>%
-  left_join(g) %>%
-  group_by(month, utm, orbit) %>%
-  arrange(month, utm, orbit, cloudCov) %>%
-  mutate(
-    prob = 1 - cumprod(cloudCov / 100),
-    n = row_number()
-  )
-# Here and now how many {month, utm tile, orbit} have data
-months %>%
-  group_by(month, utm, orbit) %>%
-  summarize(passed = any(atmCorr > 0)) %>%
-  group_by(passed) %>%
-  summarize(n = n())
-# How many {month, utm tile, orbit} can pass the minProb using no more then maxGranules?
-months %>%
-  group_by(month, utm, orbit) %>%
-  arrange(month, utm, orbit, cloudCov) %>%
-  summarize(passed = first(cloudCov) <= maxCc * 100) %>%
-  group_by(passed) %>%
-  summarize(n = n())
-# How many additional granules have to be processed
-months %>%
-  filter((n <= maxGranules | !is.na(cube)) & cloudCov <= maxCc * 100) %>%
-  group_by(atmCorr) %>%
-  summarize(n = n())
-
-### create ROIs
-
-maxGranules = 1 # per orbit
-maxCc = 0.2
-monthMin = date2months('2019-01') #min(g$month)
-monthMax = date2months('2019-12') #max(g$month)
-
-months = tibble(month = seq(monthMin, monthMax)) %>%
-  left_join(g) %>%
-  group_by(month, utm, orbit) %>%
-  arrange(month, utm, orbit, cloudCov) %>%
-  mutate(n = row_number())
-toAdd = months %>%
-  filter(n <= maxGranules & cloudCov <= maxCc * 100 & atmCorr == 0) %>%
-  mutate(bought = FALSE)
+i = list()
+for (month in monthMin:monthMax) {
+  cat(months2date(month), '\n')
+  i[[length(i) + 1]] = S2_query_image(regionId = roiEu$regionId, cloudCovMax = 50, dateMin = months2date(month), dateMax = months2date(month, end = TRUE)) %>%
+    as_tibble()
+}
+i = bind_rows(i)
+i = i %>%
+  semi_join(i %>% group_by(band) %>% summarize(resolution = min(resolution)) %>% ungroup()) %>%
+  filter(band %in% bands)
 regions = S2_query_roi(regionId = 'granule_%') %>%
   select(regionId) %>%
   mutate(granuleId = as.integer(substring(regionId, 9)))
+g = i %>%
+  group_by(granuleId, date, utm, orbit, cloudCov) %>%
+  filter(processDate == max(processDate)) %>%
+  summarize(
+    processed = sum(atmCorr == max(atmCorr) & atmCorr > 0) == length(bands),
+    atmCorr = max(atmCorr),
+    owned = all(!is.na(url[atmCorr == max(atmCorr)]))
+  ) %>%
+  ungroup() %>%
+  mutate(
+    month = date2months(date)
+  ) %>%
+  left_join(regions)
+save(i, g, file = 'all_granules.RData')
+
+muo = g %>%
+  select(month, utm, orbit) %>%
+  distinct() %>%
+  tidyr::complete()
+muo = muo %>%
+  left_join(g) %>%
+  group_by(month, utm, orbit) %>%
+  arrange(cloudCov) %>%
+  summarize(
+    nAcq = n(),
+    nProcessed = coalesce(sum(processed), 0L),
+    nOwned = coalesce(sum(owned), 0L),
+    nReady = coalesce(sum(processed & owned), 0L),
+    cc1 = cloudCov[1],
+    cc2 = lead(cloudCov)[1],
+    cc3 = lead(cloudCov, 2)[1],
+    cc4 = lead(cloudCov, 3)[1],
+  ) %>%
+  ungroup() %>%
+  mutate(
+    ccc1 = cc1,
+    ccc2 = cc1 * cc2 / 100,
+    ccc3 = cc1 * cc2 * cc3 / 10000,
+    ccc4 = cc1 * cc2 * cc3 * cc4 / 1000000,
+    ccThresh = (roiEu$dwnldThresh / 100) ^ 4 * 100
+  ) %>%
+  mutate(
+    includeRank = case_when(ccc3 > ccThresh ~ 4, ccc2 > ccThresh ~ 3, TRUE ~ 2)
+  )
+
+### stats
+stats = table(c('not ready', 'ready')[(muo$nReady > 0L) + 1L], c('no acceptable cc', 'some acceptable cc')[(muo$cc1 <= roiEu$dwnldThresh) + 1L])
+stats                   # how many {month x utm x orbit} already have data and how many may have
+stats / nrow(muo) * 100 # same in percents
+stats2 = muo %>%                 # what is a distribution of number of acqusitions per {month x utm x orbit}
+  select(month, utm, orbit, includeRank) %>%
+  inner_join(g) %>%
+  group_by(month, utm, orbit) %>%
+  filter(row_number() <= includeRank & cloudCov < roiEu$dwnldThresh | cloudCov <= roiEu$cloudCovMax) %>%
+  summarize(n = n(), ready = sum(processed)) %>%
+  ungroup() %>%
+  select(n, ready)
+table(stats2$n)                # overall
+table(stats2$ready)            # ready
+table(stats2$n - stats2$ready) # to be processed
+c('#granules' = sum(stats2$n), '#ready' = sum(stats2$ready), '#to be processed' = sum(stats2$n) - sum(stats2$ready))
+
+### roiToCreate
+toAdd = muo %>%
+  select(month, utm, orbit, includeRank) %>%
+  inner_join(g) %>%
+  group_by(month, utm, orbit) %>%
+  arrange(month, utm, orbit, cloudCov) %>%
+  mutate(
+    wanted = row_number() <= includeRank & cloudCov < roiEu$dwnldThresh | cloudCov <= roiEu$cloudCovMax,
+    bought = !is.na(regionId)
+  ) %>%
+  ungroup()
+table(c('not wanted', 'wanted')[toAdd$wanted + 1L], c('roi', 'no roi')[is.na(toAdd$regionId) + 1L])
 toAdd = toAdd %>%
-  anti_join(regions)
+  filter(wanted & is.na(regionId))
+
 save(toAdd, file = '~/Pulpit/cube.RData')
 load('~/Pulpit/cube.RData')
-for (i in seq_along(toAdd$granuleId)) {
-  if (!toAdd$bought[i]) {
+for (j in seq_along(toAdd$granuleId)) {
+  if (!toAdd$bought[j]) {
     try({
-      S2_buy_granule(toAdd$granuleId[i], 'always')
-      toAdd$bought[i] = TRUE
+      S2_buy_granule(toAdd$granuleId[j], 'always')
+      toAdd$bought[j] = TRUE
     })
   }
-  if (i %% 50 == 0) {
-    cat(i, '\n')
+  if (j %% 50 == 0) {
+    cat(j, '\n')
     save(toAdd, file = '~/Pulpit/cube.RData')
   }
 }
